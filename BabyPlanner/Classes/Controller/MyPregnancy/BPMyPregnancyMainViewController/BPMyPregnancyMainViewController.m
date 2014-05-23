90591de094e5fac652e23021e3e7d798b2188f34
Mykola Vyshynskyi <viking2009@ua.fm>
2014-05-23 14:31:49 +0300

    Task #68 My Pregnancy: Main page
    - BPMyPregnancyMainViewController: demo for different days

--- BabyPlanner/Classes/Controller/MyPregnancy/BPMyPregnancyMainViewController/BPMyPregnancyMainViewController.m
+++ BabyPlanner/Classes/Controller/MyPregnancy/BPMyPregnancyMainViewController/BPMyPregnancyMainViewController.m
@@ -117,27 +117,30 @@
     [self.view addSubview:self.myControlsButton];
     
     // TODO: check for condition and show first or second pair of images
-    self.girlView = [[UIImageView alloc] initWithImage:[BPUtils imageNamed:@"mypregnancy_main_girl1"]];
+    self.girlView = [[UIImageView alloc] init];
     [self.view addSubview:self.girlView];
 
-    self.bubbleView = [[UIImageView alloc] initWithImage:[BPUtils imageNamed:@"mypregnancy_main_bubble1"]];
+    self.bubbleView = [[UIImageView alloc] init];
     [self.view addSubview:self.bubbleView];
     
     self.selectLabel = [[UILabel alloc] init];
     self.selectLabel.backgroundColor = [UIColor clearColor];
-    self.selectLabel.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:14];
     self.selectLabel.textColor = RGB(0, 0, 0);
     self.selectLabel.textAlignment = NSTextAlignmentCenter;
 //    self.selectLabel.layer.borderWidth = 1.0f;
 //    self.selectLabel.layer.borderColor = [UIColor redColor].CGColor;
     //    self.selectLabel.shadowColor = RGB(255, 255, 255);
     //    self.selectLabel.shadowOffset = CGSizeMake(0, -1);
-    self.selectLabel.numberOfLines = 2;
+    self.selectLabel.numberOfLines = 3;
     [self.bubbleView addSubview:self.selectLabel];
     
     UISwipeGestureRecognizer *swipeUp = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(myControlsButtonTapped)];
     swipeUp.direction = UISwipeGestureRecognizerDirectionUp;
     [self.view addGestureRecognizer:swipeUp];
+    
+    UITapGestureRecognizer *demoTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(changeDay)];
+    demoTap.numberOfTapsRequired = 2;
+    [self.view addGestureRecognizer:demoTap];
     
     self.day = 47;
 
@@ -146,6 +149,15 @@
     [self localize];
     [self customize];
     
+}
+
+- (void)changeDay {
+    if (self.day == 47)
+        self.day = 120;
+    else
+        self.day = 47;
+    
+    [self updateUI];
 }
 
 - (void)didReceiveMemoryWarning
@@ -164,7 +176,6 @@
     [super updateUI];
     
     if (self.isViewLoaded) {
-        
         NSCalendar *calendar = [NSCalendar currentCalendar];
         NSDateComponents *dateComponents = [calendar components:(NSDayCalendarUnit)
                                                        fromDate:[NSDate date]];
@@ -181,15 +192,30 @@
             self.babyView.image = [BPUtils imageNamed:@"mypregnancy_main_baby2"];
             self.babyView.frame = CGRectMake(self.infoView.left - floor(self.babyView.image.size.width/2 - self.infoView.width/2), self.infoView.top + 60.f, self.babyView.image.size.width, self.self.babyView.image.size.height);
 
-            CGFloat offset = self.view.height - self.girlView.image.size.height - self.tabBarController.tabBar.height - 27.f;
-            
-            self.girlView.frame = CGRectMake(168, offset, self.girlView.image.size.width, self.girlView.image.size.height);
+            self.girlView.image = [BPUtils imageNamed:@"mypregnancy_main_girl1"];
+            self.girlView.frame = CGRectMake(168, self.view.height - self.girlView.image.size.height - self.tabBarController.tabBar.height - 27.f, self.girlView.image.size.width, self.girlView.image.size.height);
 
-            self.bubbleView.frame = CGRectMake(64, 120.f + offset, self.bubbleView.image.size.width, self.bubbleView.image.size.height);
+            self.bubbleView.image = [BPUtils imageNamed:@"mypregnancy_main_bubble1"];
+            self.bubbleView.frame = CGRectMake(64, self.girlView.top + 120.f, self.bubbleView.image.size.width, self.bubbleView.image.size.height);
 
+            self.selectLabel.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:14];
             self.selectLabel.frame = CGRectMake(18.f, 15.f, self.bubbleView.width - 36.f, self.bubbleView.height - 20.f);
+            self.selectLabel.text = BPLocalizedString(@"Your are pregnant!");
+        } else if (self.day < 180) {
+            self.infoView.left = 120.f;
             
+            self.babyView.image = [BPUtils imageNamed:@"mypregnancy_main_baby3"];
+            self.babyView.frame = CGRectMake(self.infoView.left - floor(self.babyView.image.size.width/2 - self.infoView.width/2), self.infoView.top + 60.f, self.babyView.image.size.width, self.self.babyView.image.size.height);
             
+            self.girlView.image = [BPUtils imageNamed:@"mypregnancy_main_girl2"];
+            self.girlView.frame = CGRectMake(2, self.view.height - self.girlView.image.size.height - self.tabBarController.tabBar.height - 2.f, self.girlView.image.size.width, self.girlView.image.size.height);
+            
+            self.bubbleView.image = [BPUtils imageNamed:@"mypregnancy_main_bubble3"];
+            self.bubbleView.frame = CGRectMake(120.f, self.girlView.top, self.bubbleView.image.size.width, self.bubbleView.image.size.height);
+            
+            self.selectLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:14];
+            self.selectLabel.frame = CGRectMake(5.f, 0, self.bubbleView.width - 10.f, self.bubbleView.height - 20.f);
+            self.selectLabel.text = BPLocalizedString(@"Every time you feel kick, click on my tummy!");
         }
     }
 }
@@ -202,7 +228,12 @@
     self.secondTrimesterLabel.text = BPLocalizedString(@"2nd trimester");
     self.thirdTrimesterLabel.text = BPLocalizedString(@"3rd trimester");
     
-    self.selectLabel.text = BPLocalizedString(@"Your are pregnant!");
+    // TODO: check pregnancy week, set frames
+    if (self.day < 90) {
+        self.selectLabel.text = BPLocalizedString(@"Your are pregnant!");
+    } else if (self.day < 180) {
+        self.selectLabel.text = BPLocalizedString(@"Every time you feel kick, click on my tummy!");
+    }
 }
 
 #pragma mark - Private
